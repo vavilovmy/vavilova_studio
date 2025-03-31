d615f7896e7c261b2c5a5aa230dc7ce7104e1d3a
Mikhail <justfive74@gmail.com>
2025-03-31 14:14:47 +0500

    build ready

--- components/CarouselGallery.tsx
+++ components/CarouselGallery.tsx
@@ -8,7 +8,7 @@ const CarouselGallery = () => {
    const images = [
       "/images/carousel1.jpg",
       "/images/carousel2.jpg",
-      "/images/carousel3.jpg",
+      "/images/carousel3.png",
       "/images/carousel4.jpg",
       "/images/carousel5.jpg",
       "/images/carousel6.jpg"
